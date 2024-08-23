7c659567d3d754a15389bcd09baeace8c73bd9c0
abelloch <abelloch.es@gmail.com>
2024-08-23 09:45:24 +0200

    pet model Done

---
+++ src/main/java/com/example/animalshelter/model/Pet.java
@@ -0,0 +1,32 @@
+package com.example.animalshelter.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "pet")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pet_id")
+    private Long Id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "breed")
+    private String breed;
+
+    @Column(name = "gender")
+    private String gender;
+
+}
