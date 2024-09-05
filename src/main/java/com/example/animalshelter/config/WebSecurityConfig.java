1841602eaaaade0d027e57a824be88fc93f6d651
abelloch <abelloch.es@gmail.com>
2024-09-05 12:06:30 +0200

    merging

--- src/main/java/com/example/animalshelter/config/WebSecurityConfig.java
+++ src/main/java/com/example/animalshelter/config/WebSecurityConfig.java
@@ -72,7 +72,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://172.19.48.1:3000")  // Cambia esto por la URL de tu frontend
+                .allowedOrigins("http://172.19.48.1:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
